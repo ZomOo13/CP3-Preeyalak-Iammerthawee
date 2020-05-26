d69d1d7885e2c7b3a3614ad3261c144e3d0c8989
ZomOo13 <65339979+ZomOo13@users.noreply.github.com>
2020-05-26 15:36:43 +0700

    Exercise4_Preeyalak_I
    ส่งการบ้าน Exercise4

---
+++ Exercise4_Preeyalak_I.py
@@ -0,0 +1,11 @@
+FoundEng = 61.0
+GenBusi = 98.9
+IntroCom = 51.1
+ComPro = 77.4
+
+print("            --- Your Score ---             ")
+print()
+print("Foundation English               :", FoundEng)
+print("General Business                 :", GenBusi)
+print("Introduction to Computer Systems :", IntroCom)
+print("Computer Programming             :", ComPro)
