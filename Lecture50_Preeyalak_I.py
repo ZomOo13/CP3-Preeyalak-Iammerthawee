f38f1e6e0248d2df19bd20ceea85f45cd9a12064
ZomOo13 <65339979+ZomOo13@users.noreply.github.com>
2020-05-29 11:22:21 +0700

    Lecture50_Preeyalak_I
    ส่งการบ้าน Lecture50 สร้าง Function

---
+++ Lecture50_Preeyalak_I.py
@@ -0,0 +1,21 @@
+def plus(x,y):
+    print(x+y)
+def minus(x,y):
+    print(x-y)
+def multiply(x,y):
+    print(x*y)
+def divide(x,y):
+    print(x/y)
+print("เครื่งคำนวน (บวก, ลบ, คูณ, หาร)")
+x = int(input("x = "))
+y = int(input("y = "))
+print("ผลการบวก =")
+plus(x,y)
+print("ผลการลบ =")
+minus(x,y)
+print("ผลการคูณ =")
+multiply(x,y)
+print("ผลการหาร =")
+divide(x,y)
+
+
